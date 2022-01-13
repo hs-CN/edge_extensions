52a08ddb3ef7917a6cf1c605e7ae725404600236
黄 <hs19931202@outlook.com>
2022-01-13 10:29:36 +0800

    [restore_recently_closed_tab]:change manifest_version to 3.0

--- restore_recently_closed_tab/background.js
+++ restore_recently_closed_tab/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
     chrome.sessions.getRecentlyClosed((sessions) => {
         chrome.sessions.restore(sessions[0].tab.sessionId);
     });
